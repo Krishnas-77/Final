ef4541f4b45600e4691900e345057b5a026bf879
Krishnas-77 <kasalgaonkar@gmail.com>
2024-10-14 15:03:32 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,35 +1,71 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Before;
-import org.junit.After;
-import org.junit.Test;
 
-public class AppTest {
-
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
     private WebDriver driver;
 
-    @Before
-    public void setUp() {
-        // Set up ChromeDriver using WebDriverManager
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Setup method to initialize WebDriver.
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        // Set up WebDriver using WebDriverManager
         WebDriverManager.chromedriver().setup();
-        
-        // Initialize ChromeDriver
         driver = new ChromeDriver();
     }
 
-    @Test
-    public void testGoogleHomepageTitle() {
-        driver.get("https://www.google.com");
+    /**
+     * A simple test to verify that the homepage of a website loads successfully.
+     */
+    public void testHomePage() {
+        // Navigate to the desired URL
+        driver.get("https://www.example.com");
+        
+        // Check if the title of the page contains "Example Domain"
         String title = driver.getTitle();
-        System.out.println("Page title is: " + title);
-        // Add assertions to validate the title
+        assertTrue("The title of the page does not match!", title.contains("Example Domain"));
     }
 
-    @After
-    public void tearDown() {
+    /**
+     * Teardown method to close the browser.
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        // Close the browser
         if (driver != null) {
             driver.quit();
         }
+        super.tearDown();
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
     }
 }
